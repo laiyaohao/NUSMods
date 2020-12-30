82dbdf873c44bc1c5ae6d2472f4ec888185282cf
laiyaohao <laiyaohao96@gmail.com>
2020-12-30 23:41:44 +0800

    changed the recycler_view to black background. cardview widget is also implemented in module_row.xml

--- app/src/main/java/com/example/nusmods/NUSModsAdapter.java
+++ app/src/main/java/com/example/nusmods/NUSModsAdapter.java
@@ -7,9 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.Request;
